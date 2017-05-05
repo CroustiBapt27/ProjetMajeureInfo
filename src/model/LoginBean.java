3005aa95011ff49a2ea09c87fc98ba14742a8ea4
eliottjoulot <eliottjoulot@10.150.0.194>
2017-05-05 13:50:58 +0200

    UserSubmissionModelBean + LoginBean

---
+++ src/model/LoginBean.java
@@ -0,0 +1,24 @@
+package model;
+
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@SuppressWarnings("serial")
+@ManagedBean
+@RequestScoped
+public class LoginBean implements Serializable{
+	
+	// Propiétés :
+	private	String	login;
+	private	String	pwd;
+	
+	// Constructeur :
+	public	LoginBean() { }
+	
+	// Accesseurs noramlisés :
+	public	String getLogin() 			{ return login; }
+	public void setLogin(String login) 	{ this.login = login; }
+	public String getPwd() 				{ return pwd; }
+	public void setPwd(String pwd) 		{ this.pwd = pwd; }
+}
---
+++ src/model/UserSubmissionModelBean.java
@@ -0,0 +1,15 @@
+package model;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@SuppressWarnings("serial")
+@ManagedBean
+@RequestScoped
+public class UserSubmissionModelBean extends UserModelBean{
+	
+	public UserSubmissionModelBean() {
+	}
+
+}
+
